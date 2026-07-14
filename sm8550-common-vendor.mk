13a6a625959a6b6a719336cdabc76c11d6849a3c
raebaexxx <vadimfeda@yandex.ru>
2026-07-14 20:25:39 +0300

    remove dolby, update modem

--- sm8550-common-vendor.mk
+++ sm8550-common-vendor.mk
@@ -754,7 +754,6 @@ PRODUCT_PACKAGES += \
     libvideotxr \
     libvideoutils \
     libvmfilexfer \
-    libvmmem \
     libvppclient \
     libvppcommon \
     libvpphcp \
@@ -1027,6 +1026,7 @@ PRODUCT_PACKAGES += \
     QtiTelephonyService \
     QtiTelephony \
     ims \
+    qcrilmsgtunnel \
     androidx.camera.extensions.impl \
     uimgbalibrary \
     uimgbamanagerlibrary \
@@ -1036,6 +1036,7 @@ PRODUCT_PACKAGES += \
     com.qualcomm.qti.imscmservice-V2.0-java \
     com.qualcomm.qti.imscmservice-V2.1-java \
     com.qualcomm.qti.imscmservice-V2.2-java \
+    mirilhook \
     qcrilhook \
     vendor.qti.ims.callcapability-V1.0-java \
     vendor.qti.ims.callcapabilityaidlservice-V1-java \
@@ -1064,7 +1065,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.rcsuce-V1.1-java \
     vendor.qti.ims.rcsuce-V1.2-java \
     vendor.qti.ims.rcsuceaidlservice-V1-java \
-    xiaomi-modem-common \
     android.hardware.radio.config.xml \
     android.hardware.radio.data.xml \
     android.hardware.radio.messaging.xml \
